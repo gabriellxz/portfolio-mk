a152f8d3fe8fd420da42490a3de02bfba391e97e
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-08-13 20:06:56 -0300

    corrigindo nome de projetos

--- src/components/Main-projects.tsx
+++ src/components/Main-projects.tsx
@@ -10,12 +10,12 @@ export default function MainProjects() {
                 <div className="flex w-full gap-5 overflow-x-scroll p-5 scrollbar-thin scrollbar-thumb-purpleK-100 scrollbar-track-purpleK-200 scroll">
                     {
                         projetosArray.map((item: Projects) => (
-                            <div key={item.nome} className="bg-purpleK-100 max-w-[250px] h-[250px] min-w-[250px] rounded-lg p-5">
+                            <div key={item.nome} className="bg-purpleK-100 max-w-[250px] h-[250px] min-w-[250px] rounded-lg p-5" title={item.nome}>
                                 <div className="flex justify-center text-yellow-500">
                                     <StarIcon />
                                 </div>
                                 <div className="flex flex-col gap-5 items-center text-2xl text-white font-semibold pt-5">
-                                    <span className="w-full text-center rounded-md">{item.nome}</span>
+                                    <span className="w-full text-center rounded-md whitespace-nowrap overflow-hidden">{item.nome}</span>
                                     <a href={item.link} target="_blank" className="bg-purpleK-200 w-full rounded-md flex justify-center items-center gap-5">
                                         Detalhes <ArrowRightIcon className="w-5 text-yellow-500" />
                                     </a>
