296d3f5c2697d0909704b412744d7045e24904de
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-08-13 19:54:56 -0300

    correção nomes de trabalhos

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -6,32 +6,32 @@ export type Projects = {
 
 export const projetosArray: Projects[] = [
     {
-        nome: "Santa Ceia",
+        nome: "Cartaz - Santa Ceia",
         descricao: "Cartaz para culto de Santa Ceia",
         link: "https://www.canva.com/design/DAGCyrWEwfA/1QdpVSjlYUgq-WHaYC8-qA/edit?continue_in_browser=true"
     },
     {
-        nome: "Missões",
+        nome: "Cartaz - Culto de Missões",
         descricao: "Cartaz para culto de missões",
         link: "https://www.canva.com/design/DAF_qZVJ3gM/7-k5FYXaqIOPPbRJ65CAEQ/edit?continue_in_browser=true"
     },
     {
-        nome: "Famintu",
+        nome: "Famintu - Identidade Visual",
         descricao: "Identidade visual para uma empresa fictícia",
         link: "https://www.canva.com/design/DAGFT8IXXYw/NR04ppwRHbOa8FEohaBvgQ/edit?continue_in_browser=true"
     },
     {
-        nome: "Carrosel DS",
+        nome: "Carrosel - DS",
         descricao: "Carrosel para anunciar vagas no curso de Desenvolvimento de Sistemas",
         link: "https://www.canva.com/design/DAF2Oj1eURs/5YsR5Lphnn7flLOpDEhTIQ/edit?continue_in_browser=true"
     },
     {
-        nome: "Suco Natural",
+        nome: "Cartaz propaganda - Suco Natural",
         descricao: "Cartaz comercial",
         link: "https://www.canva.com/design/DAGGcTQLt_A/VC4bavs5cdyhw7lBPS24HQ/edit?continue_in_browser=true"
     },
     {
-        nome: "Story Hamburguer",
+        nome: "Story para divulgação - Hamburguer",
         descricao: "Design para story",
         link: "https://www.canva.com/design/DAGAnbFHDk8/Tk6mpwHcUUI25iIdVaJ_TA/edit?continue_in_browser=true"
     },
@@ -41,7 +41,7 @@ export const projetosArray: Projects[] = [
         link: "https://www.canva.com/design/DAF3u3RHTZA/GdS9osr4yJMlNYbCQHUeyg/edit?continue_in_browser=true"
     },
     {
-        nome: "Post programático",
+        nome: "Post instagram - Enem Vibes",
         descricao: "Design para posts programáticos no instagram",
         link: "https://www.canva.com/design/DAF-2F7Hbw4/Vthv12VU64pxRNmp1p_ujw/edit?continue_in_browser=true"
     },
@@ -51,17 +51,17 @@ export const projetosArray: Projects[] = [
         link: "https://www.canva.com/design/DAFnt6LOZ20/be6LrjpJBvQ209lnYm2iAw/edit?continue_in_browser=true"
     },
     {
-        nome: "Anuncio",
+        nome: "Layout site - trabalho escolar",
         descricao: "Design para anunciar ofertas de camisas",
         link: "https://www.canva.com/design/DAFjSE9Jo3w/CiYKhDn0pbWjSAOBV3G_tQ/edit?continue_in_browser=true"
     },
     {
-        nome: "hamburguer",
+        nome: "Banner - Anúncio de hamburguer",
         descricao: "Design para anunciar oferta de hamburguer",
         link: "https://www.canva.com/design/DAFobS0U-Tc/vhHtYH_lxEDN3pKDe8PW8g/edit?continue_in_browser=true"
     },
     {
-        nome: "MK Açaí",
+        nome: "MK Açaí - Layout de site",
         descricao: "Design para anunciar açaí",
         link: "https://www.canva.com/design/DAFbOO150SU/YkdeIJ9_OQsPppBNGlO37A/edit?continue_in_browser=true"
     }
